de2b3dd88f4e0b86dabe811c29c33d58a734798e
wangpengzhengwpz <2061743907@qq.com>
2017-04-07 10:36:01 +0800

    联系人列表模糊搜索

--- easeui/src/com/hyphenate/easeui/adapter/EaseContactAdapter.java
+++ easeui/src/com/hyphenate/easeui/adapter/EaseContactAdapter.java
@@ -170,14 +170,15 @@ public class EaseContactAdapter extends ArrayAdapter<User> implements SectionInd
                 results.values = copyUserList;
                 results.count = copyUserList.size();
             }else{
-                String prefixString = prefix.toString();
+                String prefixString = prefix.toString().toLowerCase();
                 final int count = mOriginalList.size();
                 final ArrayList<User> newValues = new ArrayList<User>();
                 for(int i=0;i<count;i++){
                     final User user = mOriginalList.get(i);
-                    String username = user.getMUserName();
+                    String username = user.getMUserName().toLowerCase();
+                    String usernick = user.getMUserNick().toLowerCase();
                     
-                    if(username.startsWith(prefixString)){
+                    if(username.contains(prefixString) || usernick.contains(prefixString)){
                         newValues.add(user);
                     }
                     else{
