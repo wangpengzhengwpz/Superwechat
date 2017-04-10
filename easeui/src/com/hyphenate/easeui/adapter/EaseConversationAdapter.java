9f9e7bf5afce03c796948060b465d9d896bb7fd9
wangpengzhengwpz <2061743907@qq.com>
2017-04-10 11:45:51 +0800

    会话列表模糊搜索

--- easeui/src/com/hyphenate/easeui/adapter/EaseConversationAdapter.java
+++ easeui/src/com/hyphenate/easeui/adapter/EaseConversationAdapter.java
@@ -227,13 +227,14 @@ public class EaseConversationAdapter extends ArrayAdapter<EMConversation> {
                 results.values = copyConversationList;
                 results.count = copyConversationList.size();
             } else {
-                String prefixString = prefix.toString();
+                String prefixString = prefix.toString().toLowerCase();
                 final int count = mOriginalValues.size();
                 final ArrayList<EMConversation> newValues = new ArrayList<EMConversation>();
 
                 for (int i = 0; i < count; i++) {
                     final EMConversation value = mOriginalValues.get(i);
-                    String username = value.conversationId();
+                    String username = value.conversationId().toLowerCase();
+                    String nickname = "";
                     
                     EMGroup group = EMClient.getInstance().groupManager().getGroup(username);
                     if(group != null){
@@ -241,18 +242,18 @@ public class EaseConversationAdapter extends ArrayAdapter<EMConversation> {
                     }else{
                         User user = EaseUserUtils.getAppUserInfo(username);
                         // TODO: not support Nick anymore
-//                        if(user != null && user.getNick() != null)
-//                            username = user.getNick();
+                        if(user != null && user.getMUserNick() != null)
+                            nickname = user.getMUserNick().toLowerCase();
                     }
 
                     // First match against the whole ,non-splitted value
-                    if (username.startsWith(prefixString)) {
+                    if (username.contains(prefixString) || nickname.contains(prefixString)) {
                         newValues.add(value);
-                    } else{
-                          final String[] words = username.split(" ");
-                            final int wordCount = words.length;
+                    } else {
+                        final String[] words = username.split(" ");
+                        final int wordCount = words.length;
 
-                            // Start at index 0, in case valueText starts with space(s)
+                        // Start at index 0, in case valueText starts with space(s)
                         for (String word : words) {
                             if (word.startsWith(prefixString)) {
                                 newValues.add(value);
