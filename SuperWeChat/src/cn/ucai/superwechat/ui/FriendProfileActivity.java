339487ad8ce071c8ddbeab6ed77671675c0f3891
wangpengzhengwpz <2061743907@qq.com>
2017-04-10 11:48:15 +0800

    用户详情界面视频聊天单击事件

--- SuperWeChat/src/cn/ucai/superwechat/ui/FriendProfileActivity.java
+++ SuperWeChat/src/cn/ucai/superwechat/ui/FriendProfileActivity.java
@@ -1,12 +1,15 @@
 package cn.ucai.superwechat.ui;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.hyphenate.chat.EMClient;
 import com.hyphenate.easeui.domain.User;
 import com.hyphenate.easeui.utils.EaseUserUtils;
 import com.hyphenate.easeui.widget.EaseTitleBar;
@@ -119,6 +122,22 @@ public class FriendProfileActivity extends BaseActivity {
     public void sendMsg() {
         finish();
         MFGT.gotoChat(FriendProfileActivity.this, user.getMUserName());
+    }
+
+    /**
+     * make a video call
+     */
+    @OnClick(R.id.btn_send_video)
+    public void startVideoCall() {
+        if (!EMClient.getInstance().isConnected())
+            Toast.makeText(FriendProfileActivity.this, R.string.not_connect_to_server,
+                    Toast.LENGTH_SHORT).show();
+        else {
+            startActivity(new Intent(FriendProfileActivity.this, VideoCallActivity.class)
+                    .putExtra("username", user.getMUserName())
+                    .putExtra("isComingCall", false));
+            // videoCallBtn.setEnabled(false);
+        }
     }
 
     private void syncUserInfo() {
